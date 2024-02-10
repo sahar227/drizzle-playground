ea37efcf81fcc5207b50a6e7880397946d73e276
Sahar <sahar22798@gmail.com>
2024-02-10 07:35:52 +0200

    Added schema validation to my environment variables

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,16 @@
 import { config } from "dotenv";
+import { z } from "zod";
 if (process.env.NODE_ENV === "development") config();
 
-export const connectionString = process.env.CONNECTION_STRING;
+const environmentSchema = z.object({
+  NODE_ENV: z.union([
+    z.literal("production"),
+    z.literal("development"),
+    z.literal(undefined),
+  ]),
+  CONNECTION_STRING: z.string(),
+});
+
+const environment = environmentSchema.parse(process.env);
+
+export const connectionString = environment.CONNECTION_STRING;
